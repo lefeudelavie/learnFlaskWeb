a2e381578096382f3e91dd31bec5a2e3a7abe8e3
lefeudelavie <lefeudelavie@163.com>
2019-06-12 22:27:09 +0800

    4a

--- hello.py
+++ hello.py
@@ -6,15 +6,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My Secret Key!"
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route("/")
+class NewForm(FlaskForm):
+    name = StringField('What is your name?', validators= [DataRequired()])
+    submit = SubmitField('Submit')
+    
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    name = None
+    form = NewForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route("/user/<name>")
 def user(name):
@@ -28,7 +37,3 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-
-class NewForm(FlaskForm):
-    name = StringField('What is your name?', validators= [DataRequired()])
-    submit = SubmitField('Submit')
