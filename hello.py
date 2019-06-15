e125c2fde8ec536fb180a526cbb80abae901a8e7
lefeudelavie <lefeudelavie@163.com>
2019-06-15 23:44:26 +0800

    5b

--- hello.py
+++ hello.py
@@ -25,7 +25,7 @@ class Role(db.Model):
     __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy="dynamic")
 
     def __repr__(self):
         return "<Role {}>".format(self.name)
@@ -48,12 +48,20 @@ class NewForm(FlaskForm):
 def index():
     form = NewForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash("看起来你好像改了名字！")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for("index"))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', 
+        form=form, name=session.get('name'),
+        known=session.get('known', False))
 
 @app.route("/user/<name>")
 def user(name):
