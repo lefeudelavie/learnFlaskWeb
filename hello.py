a45d1d1207243fe45d91bdcfaa073f21a6a9f39b
lefeudelavie <lefeudelavie@163.com>
2019-06-16 15:30:26 +0800

    Database migration with flask-migration

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,7 +17,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACE_MODIFICATIONS'] = False
 
+
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
