43cd2790ca7f1524351474d4dff0e2b3b4f4eca4
lefeudelavie <lefeudelavie@163.com>
2019-06-23 23:46:22 +0800

    per7a_01

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,34 @@
+"""
+view module for app
+"""
+
 from datetime import datetime
-from flask import render_template, session, redirect, url_for
-from . import main
-from .forms import NameForm
+from flask import render_template, session, redirect, url_for, current_app
 from .. import db
 from ..models import User
+from ..mail import send_mail
+from . import main
+from .forms import NameForm
+
 
 @main.route("/", methods=['GET', 'POST'])
 def index():
+    """main page route for app"""
     form = NameForm()
     if form.validate_on_submit():
-
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_mail(current_app['FLASKY_ADMIN'], 'New User',
+                          'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
         return redirect(url_for('.index'))
     return render_template('index.html',
-                           form=form, name=session.get('name',
+                           form=form, name=session.get('name'),
                            known=session.get('known', False),
-                           current_time=datetime.utcnow()))
+                           current_time=datetime.utcnow())
--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.envion.get('FLASKY_ADMIN')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACE_MODIFICATIONS = False
 
     @staticmethod
@@ -22,7 +22,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
-    
+
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
@@ -36,6 +36,5 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-    
     'default': DevelopmentConfig 
 }
